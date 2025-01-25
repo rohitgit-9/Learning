8c3b7060bebedf04fb9389a678bd6f747fe46e84
rohitgit-9 <rohityadav10259@gmail.com>
2025-01-25 16:48:26 +0530

    this is first commit

---
+++ FakeApis-1/src/test/java/com/test/FakeApis1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeApis1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
